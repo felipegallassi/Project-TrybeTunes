b101436591754efc22008b75e8fe8289aa259a54
Felipe Gallassi <felipegallassi@gmail.com>
2022-03-08 15:24:16 -0300

    requisito 2 OK

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,85 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../Components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      btnDisabled: true,
+      isLoading: false,
+      redirectPag: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handlechange = this.handlechange.bind(this);
+  }
+
+  handlechange({ target }) {
+    const MIN_LENGTH_VALUE = 3;
+    this.setState({
+      [target.name]: target.value,
+    }, () => {
+      const { name } = this.state;
+      const minNameValid = name.length;
+      this.setState({
+        btnDisabled: true,
+      });
+      if (minNameValid >= MIN_LENGTH_VALUE) {
+        this.setState({
+          btnDisabled: false,
+        });
+      }
+    });
+  }
+
+  async handleClick() {
+    const { name } = this.state;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      isLoading: false,
+      redirectPag: true,
+    });
+  }
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { name, btnDisabled, isLoading, redirectPag } = this.state;
+    return (
+      <>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div data-testid="page-login">
+            <h1>Login</h1>
+            <form>
+              <input
+                data-testid="login-name-input"
+                type="text"
+                name="name"
+                id="name"
+                placeholder="digite seu nome"
+                value={ name }
+                onChange={ this.handlechange }
+              />
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                onClick={ this.handleClick }
+                disabled={ btnDisabled }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        )}
+        { redirectPag && <Redirect to="/search" /> }
+      </>
+    );
   }
 }
 
