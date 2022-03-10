8ca73f2013df1e572b8ff2d24d6c1f11602557e7
Felipe Gallassi <felipegallassi@gmail.com>
2022-03-10 16:48:08 -0300

    requisito 6 OK

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../Components/Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: '',
+      searchName: '',
       btnDisabled: true,
+      albuns: [],
+      isLoading: false,
+      inputSearch: '',
     };
     this.handleClick = this.handleClick.bind(this);
     this.handlechange = this.handlechange.bind(this);
@@ -17,8 +23,8 @@ class Search extends React.Component {
     this.setState({
       [target.name]: target.value,
     }, () => {
-      const { name } = this.state;
-      const minNameValid = name.length;
+      const { searchName } = this.state;
+      const minNameValid = searchName.length;
       this.setState({
         btnDisabled: true,
       });
@@ -30,23 +36,35 @@ class Search extends React.Component {
     });
   }
 
-  handleClick() {
-
+  async handleClick() {
+    this.setState({
+      isLoading: true,
+    });
+    const { searchName } = this.state;
+    const result = await searchAlbumsAPI(searchName);
+    console.log(result);
+    this.setState({
+      inputSearch: searchName,
+      searchName: '',
+      btnDisabled: true,
+      albuns: result,
+      isLoading: false,
+    });
   }
 
   render() {
-    const { name, btnDisabled } = this.state;
+    const { searchName, btnDisabled, isLoading, albuns, inputSearch } = this.state;
     return (
       <div data-testid="page-search">
-        <p>Search</p>
+        <Header />
         <form>
           <input
             data-testid="search-artist-input"
             type="text"
-            name="name"
-            id="name"
+            name="searchName"
+            id="searchName"
             placeholder="Nome do Artista"
-            value={ name }
+            value={ searchName }
             onChange={ this.handlechange }
           />
           <button
@@ -58,7 +76,34 @@ class Search extends React.Component {
             Pesquisar
           </button>
         </form>
-        <Header />
+        {isLoading && <Loading />}
+        {!isLoading && albuns.length === 0 && (
+          <h2>Nenhum álbum foi encontrado</h2>
+        )}
+        {!isLoading
+            && albuns.length !== 0
+              && (
+                <div>
+                  <h3>{`Resultado de álbuns de: ${inputSearch}`}</h3>
+                  <ul>
+                    {albuns.map((album) => (
+                      <li key={ album.collectionId }>
+                        <Link
+                          to={ `/album/${album.collectionId}` }
+                          data-testid={ `link-to-album-${album.collectionId}` }
+                        >
+                          <img
+                            src={ album.artworkUrl100 }
+                            alt={ `${album.collectionName} - ${album.artistName}` }
+                          />
+                          <p>{album.collectionName}</p>
+                          <p>{album.artistName}</p>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
       </div>
     );
   }
